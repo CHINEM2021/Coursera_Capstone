1b6ed357ed258e15871134d347be58ba9d6ff873
chin emei <eemei.20@gmail.com>
2021-01-23 16:21:02 +0800

    Python for Capstone

---
+++ scripts/Capstone_Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook for Capstone Project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
